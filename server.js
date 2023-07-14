8928c9e01ab6ec28947d80d25b4321d849f10888
Eric Miller <e.s.miller13125@gmail.com>
2023-07-13 21:33:07 -0600

    added api routes

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+const noteData = require('./db/db.json');
+const fs = require('fs');
 
 const PORT = 3001;
 const app = express();
 
+app.use(express.json());
 
 app.use(express.static('public'));
 
@@ -14,6 +17,29 @@ app.get('/', (req,res) =>
 app.get('/notes', (req,res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => res.json(noteData)); 
+
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+
+  const newNote = {
+    title,
+    text,
+  }
+  let dataVar;
+
+  fs.readFile('./db/db.json', (error, data) => {
+    dataVar = JSON.parse(data);
+    dataVar.push(newNote);
+    console.log(dataVar);
+    const dbArr = JSON.stringify(dataVar);
+
+    fs.writeFile('./db/db.json', dbArr, (err) =>
+      err ? console.error(err) : console.log('Success!')
+    )
+  })
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}!`)
