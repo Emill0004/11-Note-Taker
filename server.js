e870b5c4a93c06709e6a16fb6eb991c7298f3188
Eric Miller <e.s.miller13125@gmail.com>
2023-07-13 19:17:45 -0600

    added route for notes.html

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = 3001;
 const app = express();
-const path = require('path');
+
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, '/public/')));
+app.get('/', (req,res) => 
+    res.sendFile(path.join(__dirname, '/public/'))
+);
+
+app.get('/notes', (req,res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.listen(PORT, () =>
-  console.log(`Serving static asset routes at http://localhost:${PORT}!`)
+  console.log(`App listening at http://localhost:${PORT}!`)
 );
